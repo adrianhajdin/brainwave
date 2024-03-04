bba2bb144451e69f495907b3f309d5df1bf79874
uttamdasud <70854150+uttamdasud@users.noreply.github.com>
2024-03-04 23:08:27 +0530

    Fix: #4 Missing li tag in Socials list
    issue: #4
    Fixed missing li tag in social list in  Footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,14 +12,20 @@ const Footer = () => {
 
         <ul className="flex gap-5 flex-wrap">
           {socials.map((item) => (
-            <a
-              key={item.id}
-              href={item.url}
-              target="_blank"
-              className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6"
-            >
-              <img src={item.iconUrl} width={16} height={16} alt={item.title} />
-            </a>
+            <li key={item.id}>
+              <a
+                className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6"
+                href={item.url}
+                target="_blank"
+              >
+                <img
+                  src={item.iconUrl}
+                  width={16}
+                  height={16}
+                  alt={item.title}
+                />
+              </a>
+            </li>
           ))}
         </ul>
       </div>
