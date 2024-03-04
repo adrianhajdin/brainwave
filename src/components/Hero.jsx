0ebfacc7f0fb533308edc71364fb60a7446f15a6
Akrisht <akrisht.yadav@cognologix.com>
2024-03-04 16:01:04 +0530

    Issue #3 Convert / to # to reach on pricing section on page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
             Unleash the power of AI within Brainwave. Upgrade your productivity
             with Brainwave, the open AI chat app.
           </p>
-          <Button href="/pricing" white>
+          <Button href="#pricing" white>
             Get started
           </Button>
         </div>
