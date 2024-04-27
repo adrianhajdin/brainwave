56b89b3a99b1d739f0c7ecb1b853f3a537370938
Gabriel Jeannot <gabriel.jeannot@uao.edu.co>
2024-04-26 21:01:17 -0400

    Pricing updates

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -4,30 +4,30 @@ import PricingList from "./PricingList";
 import { LeftLine, RightLine } from "./design/Pricing";
 
 const Pricing = () => {
-  return (
-    <Section className="overflow-hidden" id="pricing">
-      <div className="container relative z-2">
-        <Heading
-          tag="Get started"
-          title="Pricing plans for April 2024"
-        />
+    return (
+        <Section className="overflow-hidden" id="pricing">
+            <div className="container relative z-2">
+                <Heading tag="Get started" title="Pricing plans for April 2024" />
+                <div className="flex justify-center mt-10 pb-10">
+                    <a className="text-s font-code font-bold tracking-wider uppercase border-b text-color-6">
+                        ACT NOW! This special discount pricing ends May 1, 2024
+                    </a>
+                </div>
 
-        <div className="relative">
-          <PricingList />
-          <LeftLine />
-          <RightLine />
-        </div>
+                <div className="relative">
+                    <PricingList />
+                    <LeftLine />
+                    <RightLine />
+                </div>
 
-        <div className="flex justify-center mt-10 pb-10">
-          <a
-            className="text-s font-code font-bold tracking-wider uppercase border-b"
-          >
-            More features and plans will be available soon! This is just the beginning.
-          </a>
-        </div>
-      </div>
-    </Section>
-  );
+                <div className="flex justify-center mt-10 pb-10">
+                    <a className="text-s font-code font-bold tracking-wider uppercase border-b">
+                        More features and plans will be available soon! This is just the beginning.
+                    </a>
+                </div>
+            </div>
+        </Section>
+    );
 };
 
 export default Pricing;
