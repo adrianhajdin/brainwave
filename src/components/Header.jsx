1df2b12976664ed41cfd019194e61f20434319a4
Duality <118244734+Duality404@users.noreply.github.com>
2024-07-13 10:02:19 +0530

    initial commit
    hello

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src={brainwave} width={190} height={40} alt="Brainwave" />
+          <img src={brainwave} width={190} height={40} alt="MirageN" />
         </a>
 
         <nav
