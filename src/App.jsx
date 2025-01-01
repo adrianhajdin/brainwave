8af18532817b79e13383771dc1827dc517aee25e
David-0705 <david0705eluvathingal@gmail.com>
2025-01-02 02:35:18 +0530

    Put the Grid for the events section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
+import Grid from './components/Euphoria/Grid';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Grid />
         <Benefits />
         <Collaboration />
         <Services />
